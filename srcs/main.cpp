4c5f6898cfbd2620a7a9a9521ed9a327d12ce9ea
Victorien <victorien.fischer@epitech.eu>
2017-05-23 17:32:34 +0200

    modified main so it works with Win32 (vs).

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,20 +6,37 @@
 #include <exception>
 #include "Bomberman/Bomberman.hh"
 
-int main()
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
+#else
+	int main()
+#endif
 {
     Indie::Bomberman    bomberman;
 
     try {
         if (!bomberman.loadApp())
         {
-            std::cerr << "Can't load app" << std::endl;
-            return 1;
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+			MessageBox(NULL, "Error", "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+			std::cerr << "Can't load app" << std::endl;
+#endif
+			return 1;
         }
         bomberman.runApp();
         return 0;
     } catch (std::exception& e) {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+		MessageBox(NULL, e.what(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
         std::cerr << e.what() << std::endl;
+#endif
     }
     return 0;
 }
