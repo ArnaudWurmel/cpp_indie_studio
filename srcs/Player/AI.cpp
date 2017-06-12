2532588d370f5cd38c8af4d4de9115ca1532cdf5
ballet <erwan.ballet@epitech.eu>
2017-06-12 15:53:57 +0200

    PPPPPPPPPPPPPPPPPPPPUUUUUUUUUUUUUUUUUUUUTTTTTTTTTTTTTTTTTTTEEEEEEEEEEEEEEEEEE D4AI

--- srcs/Player/AI.cpp
+++ srcs/Player/AI.cpp
@@ -10,6 +10,33 @@
 #include "../MapParser/MapParser.hh"
 #include "../Entities/EntityManager.hh"
 
+
+/*
+** Tableau de pointer sur fonction pour trouver l'heuristique
+*/
+
+const std::vector<Indie::AI::fncPtr> Indie::AI::fncTab = {
+        &Indie::AI::isForward,
+        &Indie::AI::isBackward,
+        &Indie::AI::isLeft,
+        &Indie::AI::isRight
+};
+
+/*
+**  Tableau de pointer sur fonction pour changer la position en fonction de la direction
+*/
+
+const std::vector<Indie::AI::posPtr> Indie::AI::posFncTab = {
+        &Indie::AI::addForward,
+        &Indie::AI::addBackward,
+        &Indie::AI::addLeft,
+        &Indie::AI::addRight
+};
+
+/*
+**  Constructeur
+*/
+
 Indie::AI::AI(Ogre::Vector3 const &entityPos, Ogre::SceneManager *sceneManager, std::string const &pId,
               bool mainP) : APlayer(PlayerType::IA, entityPos, sceneManager, "human.mesh", mainP)
 {
@@ -19,8 +46,94 @@ Indie::AI::AI(Ogre::Vector3 const &entityPos, Ogre::SceneManager *sceneManager, 
     addParticlesColor("Particles/Grey");
     _pId = pId;
     createMap();
-    drawMap();
 }
+
+/*
+**  Les fonctions qui determine l'heuristique
+*/
+
+bool    Indie::AI::isForward() {
+    if (_myPos.first < _enemyPos.first && _myPos.second <= _enemyPos.second) {
+        _heuristic[0] = FORWARD;
+        _heuristic[1] = RIGHT;
+        _heuristic[2] = LEFT;
+        _heuristic[3] = BACKWARD;
+        return true;
+    } else
+        return false;
+}
+
+bool    Indie::AI::isBackward() {
+    if (_myPos.first > _enemyPos.first && _myPos.second >= _enemyPos.second)
+    {
+        _heuristic[0] = BACKWARD;
+        _heuristic[1] = LEFT;
+        _heuristic[2] = RIGHT;
+        _heuristic[3] = FORWARD;
+        return true;
+    } else
+        return false;
+}
+
+bool    Indie::AI::isLeft() {
+    if (_myPos.first <= _enemyPos.first && _myPos.second > _enemyPos.second) {
+        _heuristic[0] = LEFT;
+        _heuristic[1] = FORWARD;
+        _heuristic[2] = BACKWARD;
+        _heuristic[3] = RIGHT;
+        return true;
+    } else
+        return false;
+}
+
+bool    Indie::AI::isRight() {
+    if (_myPos.first >= _enemyPos.first && _myPos.second <= _enemyPos.second) {
+        _heuristic[0] = RIGHT;
+        _heuristic[1] = BACKWARD;
+        _heuristic[2] = FORWARD;
+        _heuristic[3] = LEFT;
+        return true;
+    } else
+    return false;
+}
+
+/*
+**  Les fonctions pour changer la position en fonction de la direction
+*/
+
+bool    Indie::AI::addForward(std::pair<unsigned int, unsigned int> &pos, Indie::AI::dir direction) {
+    if (direction == FORWARD) {
+        pos.first += 1;
+        return true;
+    } else
+        return false;
+}
+
+bool    Indie::AI::addBackward(std::pair<unsigned int, unsigned int> &pos, Indie::AI::dir direction) {
+    if (direction == BACKWARD) {
+        pos.first -= 1;
+        return true;
+    } else
+        return false;
+}
+
+bool    Indie::AI::addRight(std::pair<unsigned int, unsigned int> &pos, Indie::AI::dir direction) {
+    if (direction == RIGHT) {
+        pos.second += 1;
+    } else
+        return false;
+}
+
+bool    Indie::AI::addLeft(std::pair<unsigned int, unsigned int> &pos, Indie::AI::dir direction) {
+    if (direction == LEFT) {
+        pos.second -= 1;
+    } else
+        return false;
+}
+
+/*
+** Fonction d'initialisation de la map. Rempli la map de 0 avec la bonne taille (height * width)
+*/
 
 void    Indie::AI::initMap(unsigned long height, unsigned long width) {
     unsigned long i;
@@ -31,14 +144,48 @@ void    Indie::AI::initMap(unsigned long height, unsigned long width) {
     while (i < height)
     {
         j = 0;
+        _map.push_back(std::vector<Indie::AI::map>());
         while (j < width)
         {
-            _map[i][j] = EMPTY;
+            _map[i].push_back(Indie::AI::EMPTY);
             j += 1;
         }
         i += 1;
     }
 }
+
+/*
+**  Fonction de reset de la map. Remple la map de 0
+*/
+
+void    Indie::AI::resetMap()
+{
+    unsigned long   height;
+    unsigned long   width;
+    unsigned long   i;
+    unsigned long   j;
+
+    height = _map.size();
+    if (width > 0)
+        width = _map[0].size();
+    else
+        return ;
+    i = 0;
+    while (i < height)
+    {
+        j = 0;
+        while (j < width)
+        {
+            _map[i][j] = EMPTY;
+            j++;
+        }
+        i++;
+    }
+}
+
+/*
+**  Fonction d'ajout des blocks sur la map (destructible et fixe)
+*/
 
 void    Indie::AI::addBlock(std::vector<std::shared_ptr<Indie::AEntity>>& blockList)
 {
@@ -47,12 +194,16 @@ void    Indie::AI::addBlock(std::vector<std::shared_ptr<Indie::AEntity>>& blockL
     while (it != blockList.end())
     {
         if ((*it)->hittedByExplosion())
-            _map[static_cast<int>(((*it)->getPosition().x + 1000) / 100)][static_cast<int>(((*it)->getPosition().z + 1000) / 100)] = DYNAMIC_BLOCK;
+            _map[static_cast<int>(((*it)->getPosition().x + 1000) / 100) - 1][static_cast<int>(((*it)->getPosition().z + 1000) / 100) - 1] = DYNAMIC_BLOCK;
         else
-            _map[static_cast<int>(((*it)->getPosition().x + 1000) / 100)][static_cast<int>(((*it)->getPosition().z + 1000) / 100)] = STATIC_BLOCK;
+            _map[static_cast<int>(((*it)->getPosition().x + 1000) / 100) - 1][static_cast<int>(((*it)->getPosition().z + 1000) / 100) - 1] = STATIC_BLOCK;
         it++;
     }
 }
+
+/*
+**  Fonction d'ajout des joueurs sur la map (adversaire et sois-même)
+*/
 
 void    Indie::AI::addPlayers(std::vector<std::unique_ptr<Indie::APlayer>>& playerList, std::unique_ptr<Indie::APlayer>& mainPlayer)
 {
@@ -60,13 +211,17 @@ void    Indie::AI::addPlayers(std::vector<std::unique_ptr<Indie::APlayer>>& play
 
     while (it != playerList.end())
     {
-        _map[static_cast<int>(((*it)->getPosition().x + 1000) / 100)][static_cast<int>(((*it)->getPosition().z + 1000) / 100)] = ENEMY_PLAYER;
+        _map[static_cast<int>(((*it)->getPosition().x + 1000) / 100) - 1][static_cast<int>(((*it)->getPosition().z + 1000) / 100) - 1] = ENEMY_PLAYER;
         it++;
     }
-    _map[static_cast<int>((mainPlayer->getPosition().x + 1000) / 100)][static_cast<int>((mainPlayer->getPosition().z + 1000) / 100)] = AI_PLAYER;
-    _myPos.first = static_cast<unsigned int>((mainPlayer->getPosition().x + 1000) / 100);
-    _myPos.second = static_cast<unsigned int>((mainPlayer->getPosition().z + 1000) / 100);
+    _map[static_cast<int>((mainPlayer->getPosition().x + 1000) / 100) - 1][static_cast<int>((mainPlayer->getPosition().z + 1000) / 100) - 1] = AI_PLAYER;
+    _myPos.first = static_cast<unsigned int>((mainPlayer->getPosition().x + 1000) / 100 - 1);
+    _myPos.second = static_cast<unsigned int>((mainPlayer->getPosition().z + 1000) / 100 - 1);
 }
+
+/*
+**  Fonction de création de la map (marche que si la map n'a jamais été créée)
+*/
 
 void    Indie::AI::createMap()
 {
@@ -85,7 +240,11 @@ void    Indie::AI::createMap()
     addPlayers(Indie::EntityManager::getPlayerList(), Indie::EntityManager::getMainPlayer());
 }
 
-void    Indie::AI::findEnemy() {
+/*
+**  Trouve le premier ennemi sur la map
+*/
+
+bool    Indie::AI::findEnemy() {
     std::vector<std::vector<Indie::AI::map>>::iterator     it;
     std::vector<Indie::AI::map>::iterator  it2;
     unsigned int    x;
@@ -102,7 +261,7 @@ void    Indie::AI::findEnemy() {
             if ((*it2) == ENEMY_PLAYER) {
                 _enemyPos.first = x;
                 _enemyPos.second = z;
-                return ;
+                return true;
             }
             it2++;
             z += 1;
@@ -110,10 +269,16 @@ void    Indie::AI::findEnemy() {
         it++;
         x += 1;
     }
+    return false;
 }
+
+/*
+**  Utilisation du A* pour trouver le chemin vers l'ennemi
+*/
 
 void    Indie::AI::findPath() {
     std::stack<std::pair<unsigned int, unsigned int>>   path;
+    std::stack<std::pair<unsigned int, unsigned int>>   node;
     std::pair<unsigned int, unsigned int>   currentPos;
     std::vector<std::vector<map>>     tmpMap;
     bool    end;
@@ -121,12 +286,45 @@ void    Indie::AI::findPath() {
     tmpMap = _map;
     end = false;
     currentPos = _myPos;
+    path.push(currentPos);
+    if (isANode(currentPos))
+        node.push(currentPos);
+    tmpMap[currentPos.first][currentPos.second] = ALREADY_PASS;
+    findHeuristic();
     while (!end)
     {
+        if (findNextPos(currentPos)) {
+            path.push(currentPos);
+            if (isANode(currentPos))
+                node.push(currentPos);
+            tmpMap[currentPos.first][currentPos.second] = ALREADY_PASS;
+        } else if (node.size() > 0) {
+            currentPos = node.top();
+            node.pop();
+        } else
+            end = true;
         if (currentPos == _enemyPos)
             end = true;
     }
+    while (path.size() > 0)
+    {
+        _path.push_back(path.top());
+        path.pop();
+    }
+    std::reverse(_path.begin(), _path.end());
 }
+
+/*
+**  Fonction determinant le path pour éviter une bomb
+*/
+
+void    Indie::AI::escapeBomb() {
+    
+}
+
+/*
+**  Affiche la map en askii (fonction de debug)
+*/
 
 void    Indie::AI::drawMap() {
     std::vector<std::vector<map>>::iterator     it1;
@@ -145,8 +343,83 @@ void    Indie::AI::drawMap() {
         it1++;
         std::cout << std::endl;
     }
-    std::cout << "\n\n=-=-=-=-=-=-=-=-=-= END =-=-=-=-=-=-=-=-=-=\n" << std::endl;
+    std::cout << "\n=-=-=-=-=-=-=-=-=-= END =-=-=-=-=-=-=-=-=-=\n" << std::endl;
 }
 
-Indie::AI::~AI() {}
+/*
+**  Fonction qui assigne l'heuristique dans la classe
+*/
 
+void    Indie::AI::findHeuristic() {
+    int i;
+
+    i = 0;
+    while (!(this->*fncTab[i])())
+        i += 1;
+}
+
+/*
+**  fonction qui dertermine la prochaine position en fonction de l'heuristique
+*/
+
+bool    Indie::AI::findNextPos(std::pair<unsigned int, unsigned int>& currentPos) {
+    int i;
+    std::pair<unsigned int, unsigned int>   nextPos;
+    std::vector<std::vector<map>>::iterator it;
+    std::vector<map>::iterator              it2;
+
+    i = 0;
+    while (i < 4)
+    {
+        nextPos = currentPos;
+        addDir(_heuristic[i], nextPos);
+        it = _map.begin() + nextPos.first;
+        if (it != _map.end())
+            it2 = (*it).begin() + nextPos.second;
+        if (it2 != (*it).end() && (*it2) != ALREADY_PASS && (*it2) != STATIC_BLOCK) {
+            currentPos = nextPos;
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+
+/*
+**  Fonction qui change la position en fonction de la direction
+*/
+
+void        Indie::AI::addDir(Indie::AI::dir direction, std::pair<unsigned int, unsigned int> &currentPos) {
+    int     i;
+
+    i = 0;
+    while (i < 4 && !(this->*posFncTab[i])(currentPos, direction))
+        i++;
+}
+
+/*
+**  Fonction qui determine si une pos est une node ou non (un croisement)
+*/
+
+bool    Indie::AI::isANode(const std::pair<unsigned int, unsigned int> &pos) {
+    std::pair<unsigned int, unsigned int>   nextPos;
+    int     nbPass;
+    int     i;
+
+    i = 0;
+    nbPass = 0;
+    while (i < 4)
+    {
+        nextPos = pos;
+        if ((this->*posFncTab[i])(nextPos, _heuristic[i]))
+            nbPass += 1;
+        i++;
+    }
+    return nbPass > 1;
+}
+
+/*
+**  Destructeur
+*/
+
+Indie::AI::~AI() {}
--- srcs/Player/AI.hh
+++ srcs/Player/AI.hh
@@ -19,31 +19,64 @@ namespace   Indie
         enum map
         {
             EMPTY = 0,
-            STATIC_BLOCK,
-            DYNAMIC_BLOCK,
-            ENEMY_PLAYER,
-            AI_PLAYER,
-            BOMB,
+            STATIC_BLOCK = 1,
+            DYNAMIC_BLOCK = 2,
+            ENEMY_PLAYER = 3,
+            AI_PLAYER = 4,
+            BOMB = 5,
+            ALREADY_PASS = 6
+        };
+        enum dir
+        {
+            FORWARD,
+            BACKWARD,
+            LEFT,
+            RIGHT
         };
 
     private:
+        using fncPtr = bool (AI::*)();
+        using posPtr = bool (AI::*)(std::pair<unsigned int, unsigned int>&, dir);
+
+    private:
+        static const std::vector<fncPtr> fncTab;
+        static const std::vector<posPtr> posFncTab;
         std::vector<std::vector<map>>     _map;
         std::pair<unsigned int, unsigned int>   _myPos;
         std::pair<unsigned int, unsigned int>   _enemyPos;
         std::vector<std::pair<unsigned int, unsigned int>>  _path;
+        dir     _heuristic[4];
 
     public:
         AI(Ogre::Vector3 const& entityPos, Ogre::SceneManager *sceneManager, std::string const& pId, bool mainP = false);
         ~AI();
 
     private:
+        bool    isForward();
+        bool    isBackward();
+        bool    isRight();
+        bool    isLeft();
+
+    private:
+        bool    addForward(std::pair<unsigned int, unsigned int>&, dir);
+        bool    addBackward(std::pair<unsigned int, unsigned int>&, dir);
+        bool    addLeft(std::pair<unsigned int, unsigned int>&, dir);
+        bool    addRight(std::pair<unsigned int, unsigned int>&, dir);
+
+    private:
         void    createMap();
         void    addPlayers(std::vector<std::unique_ptr<Indie::APlayer>>&, std::unique_ptr<Indie::APlayer>&);
         void    addBlock(std::vector<std::shared_ptr<Indie::AEntity>>& blockList);
         void    initMap(unsigned long, unsigned long);
+        void    resetMap();
         void    drawMap();
-        void    findEnemy();
+        bool    findEnemy();
         void    findPath();
+        void    findHeuristic();
+        void    addDir(dir, std::pair<unsigned int, unsigned int>&);
+        bool    findNextPos(std::pair<unsigned int, unsigned int>&);
+        bool    isANode(const std::pair<unsigned int, unsigned int>&);
+        void    escapeBomb();
     };
 }
 
